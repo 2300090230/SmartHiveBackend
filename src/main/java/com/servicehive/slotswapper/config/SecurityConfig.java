846c1bb9bb2bb4d1c30ea594574a5b2a18783275
Laxman <2300090230@kluniversity.in>
2025-10-31 09:21:42 +0530

    added another controller

--- src/main/java/com/servicehive/slotswapper/config/SecurityConfig.java
+++ src/main/java/com/servicehive/slotswapper/config/SecurityConfig.java
@@ -42,9 +42,9 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-            )
+            	    .requestMatchers("/api/auth/**", "/api/test", "/api/public/**").permitAll()
+            	    .anyRequest().authenticated()
+            	)
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
